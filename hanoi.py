1c6d7614e206489c89f1a7b7077fc7c7838aa044
Xinwei Chai <xinwei.chai@hotmail.com>
2020-03-04 08:57:00 +0800

    hanoi

---
+++ hanoi.py
@@ -0,0 +1,10 @@
+def hanoi(n, start, end, by):
+	if n==1:
+		print(str(start) + "->" + str(end))
+	else:
+		hanoi(n-1, start, by, end)
+		hanoi(1, start, end, by)
+		hanoi(n-1, by, end, start)
+
+if __name__=="__main__":
+	hanoi(3,1,3,2)
