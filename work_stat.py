7c75fb6b63f4e6ba94a8dfdf578121872c551321
Xinwei Chai <xinwei.chai@hotmail.com>
2020-06-02 21:58:08 +0800

    update final sheet

--- work_stat.py
+++ work_stat.py
@@ -9,7 +9,7 @@ y_offset = 7
 def work_statistics(fn):
     workbook = openpyxl.load_workbook(fn)
     sheet = workbook.worksheets[0]
-    rows = (sheet.max_row - y_offset) // 2 + 1
+    rows = (sheet.max_row - y_offset) // 2
     days = sheet.max_column
     for i in range(rows):
         absence_count = 0
@@ -43,11 +43,56 @@ def work_statistics(fn):
     workbook.save("res.xlsx")
 
 
+def count_days(fn, workbook2):
+    workbook = openpyxl.load_workbook(fn)
+    sheet1 = workbook.worksheets[0]
+    sheet2 = workbook2.worksheets[0]
+    pos_header = 0
+    pos_days = 0
+    for i in range(1, sheet1.max_column + 1):
+        if sheet1.cell(1, i).value == "标题":
+            pos_header = i
+        elif "天数" in sheet1.cell(1, i).value:
+            pos_days = i
+        elif "请假类型" in sheet1.cell(1, i).value:
+            pos_type = i
+
+    rows1 = sheet1.max_row - 1
+    rows2 = sheet2.max_row - 1
+    for i in range(2, rows1 + 2):
+        if "出差" in fn:
+            x_offset = 13
+        else:
+            if sheet1.cell(i, pos_type).value == "年假":
+                x_offset = 8
+            elif sheet1.cell(i, pos_type).value == "事假":
+                x_offset = 3
+            elif sheet1.cell(i, pos_type).value == "其他":
+                x_offset = 14
+        name = sheet1.cell(i, pos_header).value
+        name = name[:-5]
+        days = float(sheet1.cell(i, pos_days).value)
+        for j in range(5, rows2 + 1):
+            if sheet2.cell(j, 2).value == name:
+                if not sheet2.cell(j, x_offset).value:
+                    sheet2.cell(j, x_offset).value = days
+                else:
+                    sheet2.cell(j, x_offset).value += days
+                break
+    return workbook2
+
+
+# def out_shift():
+
+
+
 def count_exceptions():
-    workbook = openpyxl.load_workbook("钉钉请假.xlsx")
-    x=1
+    workbook2 = openpyxl.load_workbook("考勤汇总表-最终.xlsx")
+    workbook2 = count_days("钉钉出差.xlsx", workbook2)
+    workbook2 = count_days("钉钉请假.xlsx", workbook2)
+    workbook2.save("final.xlsx")
 
 
 if __name__ == "__main__":
-    # work_statistics("考勤机原始考勤.xlsx")
+    work_statistics("考勤机原始考勤.xlsx")
     count_exceptions()
