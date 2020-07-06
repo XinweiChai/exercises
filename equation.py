e777d9e8488eefe6177475297f60d1fd673fbafb
Xinwei Chai <xinwei.chai@hotmail.com>
2020-07-06 09:07:56 +0800

    equation updated: computation of expression

--- equation.py
+++ equation.py
@@ -1,23 +1,21 @@
-from scipy.optimize import fsolve
-from numpy import array
+# from scipy.optimize import fsolve
+# from numpy import array
+#
+#
+# def func(i):
+#     a, b, x, y = i[0], i[1], i[2], i[3]
+#     return [a * x + b * y - 3,
+#             a * (x ** 2) + b * (y ** 2) - 7,
+#             a * (x ** 3) + b * (y ** 3) - 16,
+#             a * (x ** 4) + b * (y ** 4) - 42]
+#
+#
+# x = fsolve(func, array([0, 0, 0, 0]))
+# print(x[3])
+# print(x)
+# y = x[0] * (x[2] ** 5) + x[1] * (x[3] ** 5)
+# print(y)
 
-
-def func(i):
-    a, b, x, y = i[0], i[1], i[2], i[3]
-    return [a * x + b * y - 3,
-            a * (x ** 2) + b * (y ** 2) - 7,
-            a * (x ** 3) + b * (y ** 3) - 16,
-            a * (x ** 4) + b * (y ** 4) - 42]
-
-
-x = fsolve(func, array([0, 0, 0, 0]))
-print(x[3])
-print(x)
-y = x[0] * (x[2] ** 5) + x[1] * (x[3] ** 5)
-print(y)
-
-from sympy.solvers import solve
-from sympy import Symbol
 from sympy import solve, Poly, Eq, Function, exp
 from sympy.abc import x, y, a, b
 
@@ -27,4 +25,4 @@ sol = solve([a * x + b * y - 3,
              a * (x ** 4) + b * (y ** 4) - 42], dict=True)
 print(sol)
 for i in sol:
-    print(float(i[a]) * (float(i[x]) ** 5) + float(i[b]) * (float(i[y]) ** 5))
+    print(float(i[a] * i[x] ** 5 + i[b] * i[y] ** 5))
