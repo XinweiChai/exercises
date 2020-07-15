643fe734457a076cee16511acf5e045a130d85d6
Xinwei Chai <xinwei.chai@hotmail.com>
2020-07-15 17:05:47 +0800

    add function plot

--- plot.py
+++ plot.py
@@ -23,9 +23,42 @@ import matplotlib.pyplot as plt
 # ax.legend(loc='lower right')
 #
 # plt.show()
+fig, ax = plt.subplots()
+x = np.linspace(0, 5.5, 1000)
+cond = [True if i > 1 else False for i in x]
+y = (1 - np.exp(-0.5 * (x - 1))) * cond
+z = 1 + 0 * x
+plt.plot(x, y, label='$1-e^{-k(x-a)}$')
+plt.plot(x, z, '--')
+plt.legend(fontsize='large')
+plt.ylim(0, 1.1)
+plt.xticks(np.arange(0, 6, step=1), fontsize='large')
+plt.yticks(fontsize='large')
+x1 = np.array(range(4)) + 2
+y1 = 1 - np.exp(-0.5 * (x1 - 1))
+for i, j in zip(x1, y1):
+    ax.plot(x1, y1, 'o')
+    # plt.text(i, j, '%.2f' % j,)
+    ax.annotate('%.2f' % j, (i, j), xytext=(i + 0.05, j - 0.05), fontsize='large')
+fig.tight_layout()
+plt.show()
 
-x = np.linspace(0, 10, 1000)
-y = np.sin(x)
-plt.plot(x, y, 'ro', label='$\sin(x)$', color='red', linewidth=2)
-plt.legend()
+fig, ax = plt.subplots()
+x = np.linspace(0, 6, 1000)
+cond = [True if i > 1 else False for i in x]
+y = (x - 1) / 5 * cond
+z = 1 + 0 * x
+plt.plot(x, y, label='$\\dfrac{x-1}{5}$')
+plt.plot(x, z, '--')
+plt.legend(fontsize='large')
+plt.ylim(0, 1.1)
+plt.xticks(np.arange(0, 7, step=1), fontsize='large')
+plt.yticks(fontsize='large')
+x1 = np.array(range(4)) + 2
+y1 = (x1 - 1) / 5
+for i, j in zip(x1, y1):
+    ax.plot(x1, y1, 'o')
+    # plt.text(i, j, '%.2f' % j,)
+    ax.annotate('%.2f' % j, (i, j), xytext=(i + 0.05, j - 0.05), fontsize='large')
+fig.tight_layout()
 plt.show()
