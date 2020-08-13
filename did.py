c6ab267b2271c1904ac744a58ae83ac46a8b4824
Xinwei Chai <xinwei.chai@hotmail.com>
2020-08-13 11:26:01 +0800

    did first version

--- did.py
+++ did.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+import statsmodels.api as sm
+
 
 def time(string, form="%Y-%m-%d %H:%M:%S"):
     return datetime.strptime(string, form)
@@ -37,10 +40,19 @@ def features(df, year):
     df.loc[df['during'].isnull(), 'during'] = 0
     df.loc[df["date"] >= mitigate, "after"] = 1
     df.loc[df['after'].isnull(), 'after'] = 0
+    df.loc[df["date"].dt.year == 2020, "year_2020"] = 1
+    df.loc[df['year_2020'].isnull(), 'year_2020'] = 0
     df = df.drop(columns=['date'])
-    df[['weekend', 'newyear', 'before', 'during', 'after']] = df[
-        ['weekend', 'newyear', 'before', 'during', 'after']].astype(int)
+    # df[['weekend', 'newyear', 'before', 'during', 'after']] = df[
+    #     ['weekend', 'newyear', 'before', 'during', 'after']].astype(int)
     df.to_csv(f"feature_{year}.csv", index=False)
+
+
+def combine():
+    df1 = pd.read_csv("feature_2019.csv")
+    df2 = pd.read_csv("feature_2020.csv")
+    x = pd.concat([df1, df2])
+    x.to_csv("combine.csv", index=False)
 
 
 if __name__ == '__main__':
@@ -48,9 +60,16 @@ if __name__ == '__main__':
     # for i in [2019, 2020]:
     #     df = pd.read_csv(f"{i}.csv", parse_dates=['date'], date_parser=lambda x: date(x))
     #     features(df, i)
+    # combine()
     x = pd.read_csv("combine.csv")
-    Y = np.array(x['count'].values)
-    X = np.array(x.drop(drop=['count']).values)
-    reg = LinearRegression().fit(X, Y)
-    print(reg.score(X, Y))
+    X = x.drop(columns=['count'])
+    y = x['count']
+    # X = StandardScaler().fit_transform(X)
+
+    reg = LinearRegression().fit(X, y)
+    coeff = reg.coef_
+    print(reg.score(X, y))
     print(reg.intercept_)
+    X = sm.add_constant(X)
+    est = sm.OLS(y, X).fit()
+    print(est.summary())
