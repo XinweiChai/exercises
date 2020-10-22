babc3bba50d669db1b3514c6c5509fb4d43378c6
XinweiChai <xinwei.chai@hotmail.com>
2020-10-22 17:23:32 +0800

    Update upload_level_3.sh

--- upload_level_3.sh
+++ upload_level_3.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 shopt -s nullglob
-path=data/3-level
-
-connect="PGPASSWORD=postgres psql -h 172.168.2.164 -d aqua -U postgres"
+path=3-level
+connect="PGPASSWORD=postgres psql -h 192.168.156.35 -d aqua -U postgres"
 function cluster(){
 # for i in 100 300 500
 # do
-# 	$connect -c 'CREATE TABLE '$1'_'$i' AS SELECT * FROM (SELECT ROUND(sum(area)) AS sum_area,
+# 	eval "$connect -c 'CREATE TABLE '$1'_'$i' AS SELECT * FROM (SELECT ROUND(sum(area)) AS sum_area,
 # 	ST_ConcaveHull(ST_Collect(ST_Transform(ST_SetSRID(geom, 4326), 3857)), 0.99) AS geom, 
 # 	cid, 
 # 	count(*) as count,
 # 	ST_X(ST_Centroid(ST_ConcaveHull(ST_Collect(geom),0.99))) AS centroid_x, 
 # 	ST_Y(ST_Centroid(ST_ConcaveHull(ST_Collect(geom),0.99))) AS centroid_y 
 # 	FROM (SELECT *, ST_ClusterDBSCAN(ST_Transform(ST_SetSRID(geom, 4326), 3857), eps := 50, minpoints := 2) over () AS cid FROM '$1') cluster 
-# 	WHERE cid IS NOT NULL GROUP BY cid) area WHERE sum_area>'$i''
+# 	WHERE cid IS NOT NULL GROUP BY cid) area WHERE sum_area>'$i''"
 # done
-	$connect -c 'CREATE TABLE '$1'_cluster AS SELECT * FROM (SELECT ROUND(sum(area)) AS sum_area,
+	eval "$connect -c 'CREATE TABLE '$1'_cluster AS SELECT * FROM (SELECT ROUND(sum(area)) AS sum_area,
 	ST_ConcaveHull(ST_Collect(ST_Transform(ST_SetSRID(geom, 4326), 3857)), 0.99) AS geom, 
 	cid, 
 	count(*) as count,
 	ST_X(ST_Centroid(ST_ConcaveHull(ST_Collect(geom),0.99))) AS centroid_x, 
 	ST_Y(ST_Centroid(ST_ConcaveHull(ST_Collect(geom),0.99))) AS centroid_y 
 	FROM (SELECT *, ST_ClusterDBSCAN(ST_Transform(ST_SetSRID(geom, 4326), 3857), eps := 50, minpoints := 2) over () AS cid FROM '$1') cluster 
-	WHERE cid IS NOT NULL GROUP BY cid) area'
+	WHERE cid IS NOT NULL GROUP BY cid) area'"
 }
 
 function cluster_category(){
@@ -49,19 +48,19 @@ do
 	fi
 	str=${str% OR }
 	echo $str
-	PGPASSWORD=postgres psql -h 172.168.2.164 -d aqua -U postgres -c "CREATE TABLE $1_cluster_$one$two$three AS SELECT * FROM (SELECT ROUND(sum(area)) AS sum_area,
+	eval "$connect -c 'CREATE TABLE $1'_'cluster'_'$one$two$three AS SELECT * FROM (SELECT ROUND(sum(area)) AS sum_area,
 	ST_ConcaveHull(ST_Collect(ST_Transform(ST_SetSRID(geom, 4326), 3857)), 0.99) AS geom, 
 	cid, 
 	count(*) as count,
 	ST_X(ST_Centroid(ST_ConcaveHull(ST_Collect(geom),0.99))) AS centroid_x, 
 	ST_Y(ST_Centroid(ST_ConcaveHull(ST_Collect(geom),0.99))) AS centroid_y 
 	FROM (SELECT *, ST_ClusterDBSCAN(ST_Transform(ST_SetSRID(geom, 4326), 3857), eps := 50, minpoints := 2) over () AS cid FROM $1) cluster 
-	WHERE cid IS NOT NULL AND ($str) GROUP BY cid) area"
+	WHERE cid IS NOT NULL AND ($str) GROUP BY cid) area'"
 done
 }
 function upload(){
 	shp2pgsql -c -s 4326 $1 $2|$connect
-	# $connect -c 'UPDATE '$2' SET cat=3 WHERE cat=4'
+	# $connect -c 'UPDATE $2 SET cat=3 WHERE cat=4'
 }
 function drop_category(){
 for x in {1..6}
@@ -73,13 +72,13 @@ do
 	let x=x/2
 	let three=x%2
 	echo $one$two$three
-	PGPASSWORD=postgres psql -h 172.168.2.164 -d aqua -U postgres -c "DROP TABLE $1_cluster_$one$two$three"
+	$connect -c "DROP TABLE $1'_'cluster'_'$one$two$three"
 done
 }
 function drop(){
 for i in 100 300 500
 do
-	PGPASSWORD=postgres psql -h 172.168.2.164 -d aqua -U postgres -c 'DROP TABLE '$1'_'$i''
+	eval "$connect -c 'DROP TABLE $1'_'$i'"
 done
 }
 for province in `ls $path`
