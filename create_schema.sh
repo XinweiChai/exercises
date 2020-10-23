3917431b993ef368b8e8b97975dae75e2e4b71ec
Xinwei Chai <xinwei.chai@hotmail.com>
2020-10-23 09:22:06 +0800

    rearrange

--- create_schema.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for level in `ls data`
-do
-	for province in `ls data/$level`
-	do
-		PGPASSWORD=postgres psql -h 172.169.0.6 -d aqua -U postgres -c 'CREATE SCHEMA '$province''
-	done
-done
