a4f859a7129a5700de3ac6153090b2719143b853
Xinwei Chai <xinwei.chai@hotmail.com>
2021-11-17 16:09:52 +0800

    update work_stat

--- work_stat/work_stat_basic.py
+++ work_stat/work_stat_basic.py
@@ -1,25 +1,20 @@
 import openpyxl
 from datetime import datetime
-
 import easygui
-
-# from tkinter import *
-# from tkinter.filedialog import askopenfilename
-# from tkinter.messagebox import showinfo
 
 default_offset = 6
 
 
-def work_statistics(fn, y_offset=default_offset, holidays=None):
+def work_statistics(fn, y_offset=default_offset, holidays=None, sheet=0):
     workbook = openpyxl.load_workbook(fn)
-    sheet = workbook.worksheets[0]
+    sheet = workbook.worksheets[sheet]
     rows = (sheet.max_row - y_offset) // 2
-    days = sheet.max_column
+    days = sum(sheet.cell(y_offset - 2, i).value is not None for i in range(1, sheet.max_column + 1))
     for i in range(rows):
         absence_count = 0
         not_enough_count = 0
-        absence = ""
-        not_enough = ""
+        absence = []
+        not_enough = []
         for j in range(1, days + 1):
             if j in holidays:
                 continue
@@ -27,48 +22,32 @@ def work_statistics(fn, y_offset=default_offset, holidays=None):
             # print(x)
             if not x or len(x) < 10:
                 absence_count += 1
-                absence += str(j) + ' '
+                absence.append(str(j))
                 continue
             start = datetime.strptime(x[0:5], "%H:%M")
             end = datetime.strptime(x[-5:], "%H:%M")
             if (end - start).seconds < 3600:
                 absence_count += 1
-                absence += str(j) + ' '
+                absence.append(str(j))
             # elif (end - start).seconds < 9 * 3600 - 6 * 60:
             elif start > datetime.strptime('08:35', '%H:%M') or end < datetime.strptime('17:30', '%H:%M'):
                 not_enough_count += 1
-                not_enough += str(j) + ' '
+                not_enough.append(str(j))
         sheet.cell(i * 2 + y_offset, days + 1, absence_count)
-        sheet.cell(i * 2 + y_offset, days + 2, absence)
+        sheet.cell(i * 2 + y_offset, days + 2, ' '.join(absence))
         sheet.cell(i * 2 + y_offset, days + 3, not_enough_count)
-        sheet.cell(i * 2 + y_offset, days + 4, not_enough)
-    sheet.cell(1, days + 1, "缺打卡天数")
-    sheet.cell(1, days + 2, "缺打卡日期")
-    sheet.cell(1, days + 3, "上班时间不足天数")
-    sheet.cell(1, days + 4, "上班时间不足日期")
+        sheet.cell(i * 2 + y_offset, days + 4, ' '.join(not_enough))
+    sheet.cell(1, sheet.max_column + 1, "缺打卡天数")
+    sheet.cell(1, sheet.max_column + 1, "缺打卡日期")
+    sheet.cell(1, sheet.max_column + 1, "上班时间不足天数")
+    sheet.cell(1, sheet.max_column + 1, "上班时间不足日期")
     workbook.save("res.xlsx")
 
 
 def openfile():
-    # frameT = Tk()
-    # frameT.geometry('500x100+400+200')
-    # frameT.title('Select file')
-    # frame = Frame(frameT)
-    # frame.pack(padx=10, pady=10)
-    # frame1 = Frame(frameT)
-    # frame1.pack(padx=10, pady=10)
-    # v = StringVar()
-    # ent = Entry(frame, width=50, textvariable=v).pack(fill=X, side=LEFT)
-    # # btn = Button(frame, width=20, text='Choose file', command=openpyxl.load_workbook).pack(fill=X, padx=10)
-    # ext = Button(frame, width=10, text='Run', command=work_statistics).pack(fill=X, side=LEFT)
-    # etb = Button(frame, width=10, text='Exit', command=frameT.quit()).pack(fill=Y, padx=10)
-    # frameT.mainloop()
-    # v.set('')
-    # file_name = askopenfilename()
-    # if file_name:
-    #     v.set(file_name)
+    sheet = int(easygui.enterbox('Sheet', default='0'))
     offset = easygui.enterbox('Y-offset', default='6')
-    holidays = easygui.enterbox('Holidays split by space', default='6 7 13 14 20 21 25 26 27')
+    holidays = easygui.enterbox('Holidays split by space', default='1 2 3 4 5 10 16 17 23 24 30 31')
     if holidays:
         holidays = set(map(int, holidays.split()))
     if offset and offset.isdigit():
@@ -77,7 +56,7 @@ def openfile():
         offset = default_offset
     fn = easygui.fileopenbox()
     if fn and holidays:
-        work_statistics(fn, offset, holidays)
+        work_statistics(fn, offset, holidays, sheet)
 
 
 if __name__ == "__main__":
