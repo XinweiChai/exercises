486c85a919ce239f8e9855b65c49edc2e6c56e36
Xinwei Chai <xinwei.chai@hotmail.com>
2022-05-19 20:11:12 +0800

    book_search.py

--- book_search.py
+++ book_search.py
@@ -1,15 +1,21 @@
+import os
+import webbrowser
+
 import requests as rq
+from bs4 import BeautifulSoup
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36'}
 
-books = ['军人的眼光']
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36'}
+books = []
+
+with open('C:\\Users\\Lenovo\\Desktop\\book_list.txt', encoding='utf-8') as f:
+    books += f.readlines()
 
 sites = {'京东': 'https://search.jd.com/Search?keyword=',
          '当当': 'http://search.dangdang.com/?key=',
-         '淘宝': '',
+         '淘宝': 'https://s.taobao.com/search?q=',
          '孔夫子': 'https://search.kongfz.com/product_result/?key=',
-         '有路网': 'https://www.youlu.net/search/result3/bookName=',
+         '有路网': 'https://www.youlu.net/search/result3/?bookName=',
          '多抓鱼': 'https://www.duozhuayu.com/search/book/',
          # '拼多多': '',
          # '转转': '',
@@ -23,17 +29,37 @@ sites = {'京东': 'https://search.jd.com/Search?keyword=',
          '布衣书局': 'http://www.booyee.com.cn/searchresult.jsp?bookname=',
          # '渔书': '',
          '典书旧书网': 'https://dianshu.diytrade.com/sdp/2962676/2/pl-7809337/0.html?qs=',
-         '苏宁易购': 'https://search.suning.com//',
+         '苏宁易购': 'https://search.suning.com/',
          '亚马逊': 'https://www.amazon.cn/s?k=',
          }
 
-for i in sites:
-    for book in books:
-        res = rq.get(sites[i] + book)
-        content = res.text
+
+def browsers():
+    for i in sites:
+        for book in books:
+            if i == '中国图书网':
+                book = book.encode('unicode_escape').decode().replace('\\', '%')
+            url = sites[i] + book
+            if i == '苏宁易购':
+                url += '/'
+            webbrowser.open(url, new=2)
 
 
 def post_exception():
-    res = rq.post('http://www.iisbn.com/search.html', params={'keyword': 'adobe'})
-    content = res.text
-    a = 1
+    if not os.path.exists('temp'):
+        os.mkdir('temp')
+    for book in books:
+        res = rq.post('http://www.iisbn.com/search.html', params={'keyword': book})
+        soup = BeautifulSoup(res.content, 'html.parser')
+        lst = soup.find_all('section', class_='bookscats')[0]
+        items = [str(i) + '\n' for i in lst.find_all('h3')]
+        if items:
+            fn = os.path.join('temp', f'{book}.txt')
+            with open(fn, 'w', encoding='utf-8') as f:
+                f.writelines(items)
+            os.system(f'notepad {fn}')
+
+
+if __name__ == '__main__':
+    browsers()
+    post_exception()
