175814860e5d6469f2528531fffef239bdde6abd
Xinwei Chai <xinwei.chai@hotmail.com>
2022-06-06 16:47:59 +0800

    fortran project

---
+++ fortran_project/fortran_test.py
@@ -0,0 +1,11 @@
+# import fortran
+
+# print(fortran.__doc__)
+# print(fortran.logical_to_integer.__doc__)
+# print(fortran.logical_to_integer([0, 2, 3, 4, 5], 5))
+
+
+import fortran2
+
+print(fortran2.__doc__)
+fortran2.insol()
